f7bb48dadfd0005969172504307b46ace2988fd7
caseylholston <caseylholston@gmail.com>
2017-02-17 20:39:39 -0500

    Have the first masked letter updating correctly, need to figure out how to reset the function to accept another correct letter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,28 +7,43 @@ console.log("This is the javascript file");
 				wins :0,
 				losses :0,
 			},
-			guessesLeft : 12,
-			guessesMade : 0,
-	
 		};
 	var currentWord;
+	var maskedWord =[];
 	var lettersGuessed =[];
-	var wordState = {
-		blanksLeft: 3,
-		wordLetters: ["o","n","e"]
-	};
+	var guessesMade = 0;
+	var guessesLeft = 12;
 	// Select the Current Word
+	
 	var currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-	console.log(currentWord);
+
+	//Convert Current Word into Blanks
+
+	for(i=0; i < currentWord.length; i++) {
+		maskedWord[i] = "_"		
+	}
+
 
 // When the user presses a key, it will run the following function...
-      document.onkeyup = function(event) {
+    document.onkeyup = function(event) {
 
 // Determine which key was pressed	
 	var currentGuess = event.key;
 	lettersGuessed.push(currentGuess);
 
+	var guessesLeft = (12-(lettersGuessed.length))
 	console.log(currentGuess);
+
+	for (var i = 0; i < currentWord.length; i++){
+		if(currentWord[i] === currentGuess){
+			maskedWord[i] = currentGuess + " ";
+			var correctGuess = true;
+		}
+		else if (currentGuess = "");
+	}
+
+
+
 //    var targetDiv = document.getElementById("empty-div");
 
 //       targetDiv.innerHTML = "Hello friends!";
